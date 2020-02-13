9a4101bbdde7a08bbdbce06b06a0eafe43388da5
paastry <paastry@protonmail.com>
2020-02-13 13:09:14 +0530

    first

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 //MONGO
 
@@ -46,11 +47,10 @@ const NotesSchema = new mongoose.Schema({
 const NotesModel = new mongoose.model("notesnew", NotesSchema);
 
 // const note1 = new NotesModel({
-//   title: "Mangoes",
+//   title: "Peaches",
 //   subtitle: "It's a fruit",
 //   content: "It's yellow and tasty and supposedly king of fruits"
 // });
-
 // note1.save((err,data)=>{
 // 	if(err) console.log(err);
 // 	console.log(data);
@@ -62,12 +62,21 @@ const PORT = 8072;
 
 const server = express();
 server.use(cors());
+server.use(bodyParser.json()); // to support JSON-encoded bodies
+server.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true
+  })
+);
+server.use(express.json());
 
 //API(s)
 
 // console.log(NotesModel.find());
 
 server.get("/notes", (req, res) => {
+  // console.log("notes was called");
   NotesModel.find((err, data) => {
     // console.log(data);
     res.json(data);
@@ -75,8 +84,29 @@ server.get("/notes", (req, res) => {
 });
 
 server.post("/savenote", (req, res) => {
-	console.log("-----------------------------------");
   console.log(req.body);
+  if (req.body.id) {
+    NotesModel.findOneAndUpdate(
+      { _id: req.body.id },
+      req.body,
+      { upsert: true },
+      (err, data) => {
+        if (err) console.log(err);
+        // console.log("ret data------------");
+        // console.log(data);
+        res.send({ id: `${data._id}` });
+      }
+    );
+  } else {
+    new NotesModel({
+      title: req.body.title,
+      subtitle: req.body.subtitle,
+      content: req.body.content
+    }).save((err, data) => {
+      if (err) console.log(err);
+      console.log(data);
+    });
+  }
 });
 
 server.get("/note/:id", (req, res) => {
